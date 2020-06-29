a0e1611b9451ea106fb4922ff820505149323403
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-29 13:41:15 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
+	}
+
+}
