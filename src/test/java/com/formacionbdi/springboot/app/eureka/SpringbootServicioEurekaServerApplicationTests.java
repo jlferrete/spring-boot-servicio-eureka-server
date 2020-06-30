a22564633169c0c44112159af0fb514a53bc5d57
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-30 16:47:43 +0200

    Fixed issues

--- src/test/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
+++ src/test/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.formacionbdi.springboot.app.eureka;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootServicioEurekaServerApplicationTests {
+public class SpringbootServicioEurekaServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
